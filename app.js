8f0449aaace814fc8452e87bbb0e3ac59af2e2ab
leejunseon <ljs921026@gmail.com>
2021-03-01 23:19:53 +0900

    2021-03-01

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
+app.use(helmet()); //application이 더 안전하도록 만들어줌.
 app.set("view engine", "pug");
 //cookie를 전달받아 사용할 수 있도록 만들어줌 -> 사용자 인증에서 사용
 app.use(cookieParser());
 //사용자가 웹사이트로 전달하는 정보들을 검사함.
 app.use(bodyParser.json()); //request정보에서 json형태로 된 body를 검사
 app.use(bodyParser.urlencoded({ extended: true })); //request정보에서 form형태로 된 body를 검사
-app.use(helmet()); //application이 더 안전하도록 만들어줌.
 app.use(morgan("dev")); //application에서 발생하는 모든 일들을 logging
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
