9ea34f3fae70e58d5927a6ae8582f295cb9edfea
leejunseon <ljs921026@gmail.com>
2021-03-04 21:28:21 +0900

    2021-03-04

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,13 +1,24 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  console.log(searchingBy);
+  res.render("search", { pageTitle: "Search", searchingBy: searchingBy });
+};
+
 export const videos = (req, res) =>
   res.render("videos", { pageTitle: "videos" });
+
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "upload" });
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "videoDetail" });
+
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "editVideo" });
+
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "deleteVideo" });
