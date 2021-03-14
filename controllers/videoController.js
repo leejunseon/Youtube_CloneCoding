f0441f1cadc157c3d2dd5dd74573243f1d2664b0
leejunseon <ljs921026@gmail.com>
2021-03-14 18:33:33 +0900

    2021-03-14

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,8 @@
-export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+import { videoDatas } from "../db";
+
+export const home = (req, res) => {
+  res.render("home", { pageTitle: "Home", videoDatas });
+};
 
 export const search = (req, res) => {
   const {
---
+++ db.js
@@ -0,0 +1,38 @@
+export const videoDatas = [
+  {
+    id: 324393,
+    title: "Video awesome 1",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 324394,
+    title: "Video awesome 2",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+  {
+    id: 324395,
+    title: "Video awesome 3",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com",
+    },
+  },
+];
